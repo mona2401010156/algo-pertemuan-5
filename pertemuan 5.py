602fa513b5d705ff79b78e9cc86631df83d07b93
mona2401010156 <yaniarimona@gmail.com>
2024-12-05 12:20:48 +0800

    Add files via upload

---
+++ pertemuan 5.py
@@ -0,0 +1,5 @@
+kalimat = 'stiki'
+print ('kalimat :' , kalimat)
+print ('\'i\' in kalimat:', 'i' in kalimat)
+print ('\'z\' not in kalimat:', 'z' not in kalimat)
+print ('\'d\' not in kalimat:', 'd' not in kalimat)
---
+++ pertemuan5-2.py
@@ -0,0 +1,4 @@
+nama = 'bersama'
+print ('nama [0]:', nama[0])
+print ('nama [0:3]:', nama[0:3])
+print ('nama [1:4]:', nama[1:4])
